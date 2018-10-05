4d8240ff793146ce2dd82df452821d6aaff7a6eb
Hello <31024588+DSep@users.noreply.github.com>
2018-10-04 17:55:55 -0700

    Finished day 3

--- Day2.js
+++ Day2.js
@@ -12,11 +12,11 @@ const GRID = [
 ];
 
 /*
-Write a function called countRows() which will tell us the number of rows in our GRID. Remember that other lighthouse
-operators are going to be using this function, so it has to be able to work on a GRID of any size. It would be easy
-to write a function that just did return 10; but that wouldn't help operators with bigger or smaller areas!
-
- */
+Write a function called countColumns() that will tell us the number of columns in our GRID. 
+Again, remember that this has to work for all the lighthouse operators out there, no matter 
+what size their area is. Also, your code will still be able to see the GRID variable, and you 
+won't need to pass it as an argument to the function.
+*/
 
 function countColumns() {
   return GRID[0].length;
